c07cfb04bfb3d5458a21fc9a2a625d2e3abf07ff
Javid Izadfar <Javid.Izadfar@gmail.com>
2017-06-19 23:24:28 +0430

    Added Live Time

---
+++ assets/js/master.js
@@ -0,0 +1,23 @@
+function twlv(t) {
+  if (t > 12) t = t - 12;
+  return t
+}
+
+function dbl(t) {
+  if ( t < 10 ) t = '0' + t;
+  return t
+}
+
+function updateTime() {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes()
+
+  document.querySelector('.time').innerHTML = twlv(hour) + ':' + dbl(minute);
+}
+
+docReady(function() {
+  updateTime();
+  setInterval(function(){ updateTime() }, 10000);
+
+});
